394dabbba7014abbea801dc4e40febe137a19373
Sergio Rosa <scarlosjazz@gmail.com>
2019-08-13 21:16:09 -0400

    implenting Hook useState

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>React Hooks</h1>
+  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [newTech, setNewTech] = useState(' ');
+
+
+  function handleAdd() {
+    setTech([...tech, newTech ]);
+    setNewTech(' ');
+  }
+
+  return (
+    <>
+    <ul>
+      {tech.map(t => (
+        <li key={t}>{t}</li>
+      ))}
+    </ul>
+    <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+    <button type="button" onClick={handleAdd}>Adding</button>
+    </>
+  )
 }
 export default App;
